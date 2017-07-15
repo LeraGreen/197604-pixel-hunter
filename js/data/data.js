29afcd83009c86589b6c7f07e3ad82d0c50a814d
Green Lera <zelnayavaleriya@gmail.com>
2017-07-16 00:36:09 +0300

    добавляет резайс фото

--- js/data/data.js
+++ js/data/data.js
@@ -308,4 +308,18 @@ export const createQuestions = () => {
   }
 };
 
+export const resize = (frame, given) => {
+  const scaleHeight = frame.height / given.height;
+  const scaleWidth = frame.width / given.width;
+  let scale;
+  if (scaleHeight < scaleWidth) {
+    scale = scaleHeight;
+  } else {
+    scale = scaleWidth;
+  }
+  given.width = given.width * scale;
+  given.height = given.height * scale;
+  return given;
+};
+
 
